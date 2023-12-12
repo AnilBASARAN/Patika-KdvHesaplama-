e35a5e9cdbc6e559df481bbebc4d5c319abdec34
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-12 12:37:04 +0300

    duzenleme yapıldı bu koşul bilgisi için kod eklendi.
    Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.

--- KdvHesaplama.java
+++ KdvHesaplama.java
@@ -7,9 +7,12 @@ public class KdvHesaplama {
         // productsPrice isimli değişkene kullanıcıdan aldığımız veriyi giriyoruz
         double productPrice = input.nextDouble();
         // vergi çarpanı 1.18 olarak belirlendi
-        double taxConstant = 0.18;
+        double taxConstant = (productPrice <= 1000 && productPrice >=0 )? 0.18 : 0.08 ;
         System.out.println("KDV'siz fiyat = " + productPrice);
         System.out.println("KDV'li fiyat = " + productPrice * (1+taxConstant) );
         System.out.println("KDV tutarı = " + productPrice * taxConstant );
+
+        //Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+        // tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
     }
 }
